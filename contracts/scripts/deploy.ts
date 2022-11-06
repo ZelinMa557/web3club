aca4e1d872d577dc56db0fe704753222e989c09b
ZelinMa557 <3388706467@qq.com>
2022-11-06 20:23:43 +0800

    version_2_1

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -5,8 +5,10 @@ async function main() {
   const studentSocietyDAO = await StudentSocietyDAO.deploy();
   await studentSocietyDAO.deployed();
   const erc20 = await studentSocietyDAO.studentERC20()
+  const erc721 = await studentSocietyDAO.studentERC721()
   console.log(`StudentSocietyDAO deployed to ${studentSocietyDAO.address}`);
-  console.log(`erc20 contract has been deployed successfully in ${erc20}`)
+  console.log(`erc20 contract has been deployed successfully in ${erc20}`);
+  console.log(`erc721 contract has been deployed successfully in ${erc721}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
